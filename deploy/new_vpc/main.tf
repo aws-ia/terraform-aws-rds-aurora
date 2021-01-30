c88d7f113180b0fd2dcde95799a070e73993cbc0
Tony Vattathil <avattathil@gmail.com>
2021-01-29 17:23:30 -0800

    sync name across vpc and aurora deployment

--- deploy/new_vpc/main.tf
+++ deploy/new_vpc/main.tf
@@ -18,14 +18,18 @@ resource "random_pet" "name" {
   length = 1
 }
 
+locals {
+  name = random_pet.name.id
+}
+
 ######################################
 # Create VPC
 ######################################
 
-module "quickstart_vpc" {
+module "aurora_vpc" {
   source            = "aws-quickstart/vpc/aws"
   region            = var.region
-  name              = random_pet.name.id
+  name              = local.name
   cidr              = "10.0.0.0/16"
   public_subnets    = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
   private_subnets_A = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
@@ -36,8 +40,9 @@ module "quickstart_vpc" {
 ######################################
 
 module "aurora" {
-  depends_on = [module.quickstart_vpc]
-  source     = "../../modules/aurora"
+  depends_on = [module.aurora_vpc]
+  source     = "../../modules/aurora2"
   region     = var.region
-  name       = random_pet.name.id
+  name       = local.name
+  vpc_id     = module.aurora-vpc.vpc_id
 }
