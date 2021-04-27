641e6afc43ac9061b736d884654f934bd245d036
Tony Vattathil <avattathil@gmail.com>
2021-04-27 15:52:58 -0700

    Set min terraform version to v0.15.1

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,7 +1,8 @@
-# ----------------------------------------------------------------------------------------------------------------------
-# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-# ----------------------------------------------------------------------------------------------------------------------
+terraform {
+  required_version = ">= 0.15.1"
+  backend "remote" {}
+}
+
 ######################################
 # Defaults
 ######################################
