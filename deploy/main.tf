e5a7a2a4a29ed688336f706bdb719bee1fab78da
Tony Vattathil <avattathil@gmail.com>
2021-04-28 14:28:10 -0700

    Update vpc version 0.07

--- deploy/main.tf
+++ deploy/main.tf
@@ -12,24 +12,14 @@ provider "aws" {
 }
 
 
-resource "random_pet" "name" {
-  prefix = "tfm-aws"
-  length = 1
-}
-
-locals {
-  name = random_pet.name.id
-}
-
 ######################################
 # Create VPC
 ######################################
 
 module "aurora_vpc" {
   source            = "aws-quickstart/vpc/aws"
-  version           = "0.0.6"
+  version           = "0.0.7"
   region            = var.region
-  name              = local.name
   cidr              = "10.0.0.0/16"
   public_subnets    = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
   private_subnets_A = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
@@ -43,6 +33,5 @@ module "aurora" {
   depends_on = [module.aurora_vpc]
   source     = "../modules/aurora"
   region     = var.region
-  name       = local.name
   vpc_id     = module.aurora_vpc.vpc_id
 }
