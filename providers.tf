9cae8579380872b8261dd1a9eeb772c1443bfc18
Arabinda Pani <22154314+catcharbind@users.noreply.github.com>
2022-07-20 09:37:53 -0700

    provider change log

--- providers.tf
+++ providers.tf
@@ -1,3 +1,6 @@
+# https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md
+# https://github.com/hashicorp/terraform/blob/v1.0.10/CHANGELOG.md
+
 terraform {
   required_version = ">= 1.0.0"
   backend "remote" {}
